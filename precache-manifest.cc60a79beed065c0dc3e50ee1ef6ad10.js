0f0360e0bee275cd9f9bd8c76cf020d7fbfdc174
Dima9119708 <59973263+Dima9119708@users.noreply.github.com>
2020-11-06 19:56:53 +0200

    Updates

---
+++ precache-manifest.cc60a79beed065c0dc3e50ee1ef6ad10.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6995d29928e4e0ab905d39aff2a1486b",
+    "url": "/googleDocClone/index.html"
+  },
+  {
+    "revision": "f0ccf094b92df22690bb",
+    "url": "/googleDocClone/static/css/2.2f389312.chunk.css"
+  },
+  {
+    "revision": "bc51c73829d562218b91",
+    "url": "/googleDocClone/static/css/main.ed0cadfe.chunk.css"
+  },
+  {
+    "revision": "f0ccf094b92df22690bb",
+    "url": "/googleDocClone/static/js/2.145db4ec.chunk.js"
+  },
+  {
+    "revision": "20377f3c2f2703e596dd85276960f64e",
+    "url": "/googleDocClone/static/js/2.145db4ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc51c73829d562218b91",
+    "url": "/googleDocClone/static/js/main.3f6edff1.chunk.js"
+  },
+  {
+    "revision": "ab55f662adadf5a91d2b",
+    "url": "/googleDocClone/static/js/runtime-main.9bc4c729.js"
+  }
+]);
